d46c33eddb33f987b5b002f8560b5cafb38416a0
Shubhankar Kuranagatti <shubhankar.vk@gmail.com>
2021-06-01 23:33:10 +0530

    Sanju to the rescue

--- Week5/5.c
+++ Week5/5.c
@@ -5,15 +5,14 @@ void count(char str[100])
 {
     int flag=0;
     char c;
-    for(int i=0;i<strlen(str);i++)
+    for(int i=0;i<=strlen(str);i++)
     {
-        if(i==0) // This counts the total number of spaces in the first iteration
-           c=' '; 
+        if(i==0)
+            c=' ';
         else if(str[i-1]!=' ') // This is used to omit all spaces while counting characters
             c= str[i-1];
         else
-            break;
-        flag=0;
+            goto loop;
         for(int j=0;j<strlen(str);j++)
         {   
             if(c==str[j]) // Initiates count for a certain character
@@ -23,6 +22,8 @@ void count(char str[100])
                 }
         }
         printf("%c occurs %d times\n",c,flag);
+        loop:
+        flag=0;
 
     }
 }
