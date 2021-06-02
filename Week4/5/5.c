caa654de2f6e04afcac057ef185d9fc7148ffb70
Shubhankar Kuranagatti <shubhankar.vk@gmail.com>
2021-06-02 14:44:32 +0530

    Added checking the input number to be prime or not

--- Week4/5/5.c
+++ Week4/5/5.c
@@ -3,8 +3,15 @@
 
 void main()
 {
-printf("Entre the number to be cheked:- ");
-int num;scanf("%d",&num);
+printf("Entre the number to be checked:- ");
+int num;
+scanf("%d",&num);
+if(prime(num)!=0)
+printf("%d is a prime number",num);
+else
+{
+printf("%d is not a prime number\n",num);
 printf("The next prime number is:- %d",nprime(num+1));
+}
 printf("\n");
 }
